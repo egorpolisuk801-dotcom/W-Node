e12b79e6c69f96b88e0d19fea2ee1a1bdfdcd551
Egor <egor@github.com>
2026-02-13 22:37:24 +0200

    FInal attempt: Fixed Podfile and added build cleanup

--- ios/Podfile.rb
+++ ios/Podfile.rb
@@ -1,5 +1,6 @@
 platform :ios, '13.0'
 
+# Стандартный блок подключения Flutter
 setup_flutter_pts = File.join(File.dirname(File.expand_path(__FILE__)), 'Flutter', 'generated_code_util.rb')
 load setup_flutter_pts if File.exist?(setup_flutter_pts)
 
@@ -14,6 +15,7 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     flutter_additional_ios_build_settings(target)
     target.build_configurations.each do |config|
+      # Это лечит большинство ошибок сборки на GitHub Actions
       config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
       config.build_settings['ENABLE_BITCODE'] = 'NO'
     end
